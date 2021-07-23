565c4f917e93bcf504d515f605bb919e24625ac3
shenxin <363088847@qq.com>
2021-07-23 15:10:49 +0800

    index.tsx 可以打包成 index.js 了！

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path')
+module.exports = {
+    mode: 'production',
+    entry: {
+        fui: './lib/index.tsx'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'FUI',
+        libraryTarget: 'umd',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    }
+
+}
