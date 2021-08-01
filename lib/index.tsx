1a32f651838526f2f50a9789d7f7c0649a10c29a
shenxin <363088847@qq.com>
2021-08-01 14:40:23 +0800

    onClick 类型完善

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,8 +2,8 @@ import ReactDom from 'react-dom';
 import React from 'react';
 import Icon from './icon';
 
-const fn = () => {
-    console.log('fn');
+const fn: React.MouseEventHandler = (e) => {
+    console.log(e.target);
 };
 
 ReactDom.render(
