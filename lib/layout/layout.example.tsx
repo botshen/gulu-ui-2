4f791c5c9d4b45a10f7a5017a4c21538fb66fd2d
shenxin <363088847@qq.com>
2021-08-02 22:28:41 +0800

    重构 scopedClassMaker

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -6,19 +6,19 @@ import Footer from './footer';
 import Aside from './aside';
 
 export default function () {
-  return (
-    <div>
-      <div>
-        <h1>第四个例子</h1>
-        <Layout style={{height: 500}} className="hi">
-          <Aside>aside</Aside>
-          <Layout>
-            <Header>header</Header>
-            <Content>content</Content>
-            <Footer>footer</Footer>
-          </Layout>
-        </Layout>
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <div>
+                <h1>第四个例子</h1>
+                <Layout style={{height: 500}} className="hi">
+                    <Aside>aside</Aside>
+                    <Layout>
+                        <Header>header</Header>
+                        <Content>content</Content>
+                        <Footer>footer</Footer>
+                    </Layout>
+                </Layout>
+            </div>
+        </div>
+    );
 }
