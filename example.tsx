bd38fe61f7b17237dbacd5d0a6931b92cc86452d
shenxin <363088847@qq.com>
2021-08-03 11:22:35 +0800

    完成 example 页面的布局

--- example.tsx
+++ example.tsx
@@ -5,42 +5,48 @@ import IconExample from './lib/icon/icon.example';
 import DialogExample from './lib/dialog/dialog.example';
 import ButtonExample from './lib/button.example';
 import LayoutExample from './lib/layout/layout.example';
+import {Layout, Aside, Header, Content, Footer} from './lib/layout/layout';
+import './example.scss';
 
+const logo = require('./logo.png');
 
 ReactDOM.render(
-  <Router>
-    <div>
-      <header>
-        <div className="logo">
-          FUI
-        </div>
-
-      </header>
-      <div>
-        <aside>
-          <h2>组件</h2>
-          <ul>
-            <li>
-              <Link to="/icon">Icon</Link>
-            </li>
-            <li>
-              <Link to="/button">Button</Link>
-            </li>
-            <li>
-              <Link to="/dialog">对话框</Link>
-            </li>
-            <li>
-              <Link to="/layout">布局</Link>
-            </li>
-          </ul>
-        </aside>
-        <main>
-          <Route path="/icon" component={IconExample}/>
-          <Route path="/button" component={ButtonExample}/>
-          <Route path="/dialog" component={DialogExample}/>
-          <Route path="/layout" component={LayoutExample}/>
-        </main>
-      </div>
-    </div>
-  </Router>
-  , document.querySelector('#root'));
+    <Router>
+        <Layout className="site-page">
+            <Header className="site-header">
+                <div className="logo">
+                    <img src={logo} width="48" height="48" alt=""/>
+                    <span> XINUI </span>
+                </div>
+            </Header>
+            <Layout>
+                <Aside className="site-aside">
+                    <h2>组件</h2>
+                    <ul>
+                        <li>
+                            <Link to="/icon">Icon</Link>
+                        </li>
+                        <li>
+                            <Link to="/button">Button</Link>
+                        </li>
+                        <li>
+                            <Link to="/dialog">对话框</Link>
+                        </li>
+                        <li>
+                            <Link to="/layout">布局</Link>
+                        </li>
+                    </ul>
+                </Aside>
+                <Content className="site-main">
+                    <Route path="/icon" component={IconExample}/>
+                    <Route path="/button" component={ButtonExample}/>
+                    <Route path="/dialog" component={DialogExample}/>
+                    <Route path="/layout" component={LayoutExample}/>
+                </Content>
+            </Layout>
+            <Footer className="site-footer">
+                &copy; 沈鑫
+            </Footer>
+        </Layout>
+    </Router>
+    , document.querySelector('#root'));
